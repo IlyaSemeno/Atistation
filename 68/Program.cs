9e17ac963e6bb400ab7b8f790f8a2345582ec94b
Ilya <Semenoffqwe@gmail.com>
2023-10-23 18:52:53 +0300

    68 end

--- 68/Program.cs
+++ 68/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите значение m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите значение n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = AckermannFunction(m, n);
+
+        Console.WriteLine($"A({m}, {n}) = {result}");
+ static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
