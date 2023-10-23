149293eaea94742af92662d8cfd0175c5bf482cf
Ilya <Semenoffqwe@gmail.com>
2023-10-23 18:47:10 +0300

    66 end

--- 66/Program.cs
+++ 66/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите значение M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        if (M < 1 || N < 1)
+        {
+            Console.WriteLine("Некорректный ввод!");
+            return;
+        }
+
+        int sum = CalculateSum(M, N);
+        Console.WriteLine($"Сумма натуральных чисел в промежутке от {M} до {N} равна: {sum}");
+    
+static int CalculateSum(int numM, int numN)
+    {
+        if (numM == numN)
+            return numN;
+        
+        return numM + CalculateSum(numM + 1, numN);
+    }
