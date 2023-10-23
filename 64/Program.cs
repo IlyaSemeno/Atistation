a31092b37b24d7afc8891f17214b739810f5183f
Ilya <Semenoffqwe@gmail.com>
2023-10-23 18:24:34 +0300

    64 end

--- 64/Program.cs
+++ 64/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<1){
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+PrintNaturalNumbers(number);
+void PrintNaturalNumbers(int num){
+    if (num == 0)
+            return;
+
+        Console.Write($"{num} ");
+
+        if (num > 1)
+            PrintNaturalNumbers(num - 1);
+    
+}
